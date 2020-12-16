6e9d041024598c7c022e3f0fe7285b99172f6aba
HeidyJohanaP <https://github.com/HeidyJohanaP>
2020-12-15 21:15:16 -0500

    politicas CORS

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ from fastapi import HTTPException
 import datetime
 import db.movimientos_db
 from db.movimientos_db import database_movs
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+"http://localhost", 
+"http://localhost:8080",
+"https://piggy-grow-frontend.herokuapp.com/",
+]
+app.add_middleware(
+CORSMiddleware,
+allow_origins=origins,
+allow_credentials=True, 
+allow_methods=["*"], 
+allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -42,3 +56,14 @@ async def crear_movimiento(movimiento: db.movimientos_db.Movimiento):
     else:
         raise HTTPException(status_code=400, detail="Ya existe un movimiento con el ID especificado.")
 
+#@app.get("/user")
+#async def Usuario():
+#    return usuario_db
+
+#@app.post("/user/nuevo")
+#async def crear_usuario(usuario: db.usuario_db.Usuario):
+#    usuario_creado = db.usuario_db.crear_usuario(usuario)
+#    if usuario_creado:
+#        return {"message" : "Usuario creado exitosamente."}
+#    else:
+#        raise HTTPException(status_code=400, detail="Ya existe un usuario con el Username especificado.")
